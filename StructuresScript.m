e1c47c6f401867191807da0762cb23175841126f
DaveShan1722 <hs1722@ic.ac.uk>
2024-11-26 23:08:47 +0000

    Update StructuresScript.m

--- StructuresScript.m
+++ StructuresScript.m
@@ -13,7 +13,7 @@ b18i=81;
 b12=17.4;
 b23=286.66;
 b36=81;
-T=1;%T=P*L
+T=;%T=P*L
 syms q1 q2 theta_z
 equs = [2*q1*A1+2*q2*A2==T, theta_z==1/(2*A1*G)*(q1*b18e/t18e+(q1-q2)*b18i/t18i) , theta_z==1/(2*A2*G)*(q2*(2*b23/t23+b36/t36)+(q2-q1)*b18i/t18i)];
 vars = [q1,q2,theta_z];
